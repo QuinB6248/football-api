84841275df0f728694671b0f6edff33c839afe19
QuinB6248 <4_sAfJO25N@outlook.com>
2019-06-26 12:23:41 +0200

    removed timestamp, add table

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
 const db = require('./db')
+const Team = require('./team/model')
+
 
 app.listen(port, function () {
   console.log(`Web server listening on port ${port}`)
